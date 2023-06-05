205615261dce9b53abffa63d034a57c96e4912bc
jonahyad44 <103967416+jonahyad44@users.noreply.github.com>
2023-06-05 10:34:42 +0900

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import './AddTask.jsx';
+import './Item.jsx';
 
 function App() {
+  const  getItem =  (item)  => {
+    console.log(item);
+  
   return (
     <div className="To-Do App">
+    <AddTask item= {getItem}/>
       <header className="App-header">
     <h1> To-Do List </h1>
         
