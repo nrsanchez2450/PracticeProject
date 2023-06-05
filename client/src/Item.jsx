8a24bb594e0577c5b3d34748d28b62ca50037194
jonahyad44 <103967416+jonahyad44@users.noreply.github.com>
2023-06-05 11:13:19 +0900

    Update Item.jsx

--- client/src/Item.jsx
+++ client/src/Item.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 const Item = () => {
   return (
     <div className= 'todo-item'>
-      <input type = "checkbox" onChange=() =>  props.completed()}/>
+      <input type = "checkbox" onChange={() =>  props.completed(props.id)}/>
+      <p className = 'mll'> {props.item}</p>
+</div>
 
 export default Item;
