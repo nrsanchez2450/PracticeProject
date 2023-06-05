7200d0e3d8d22fce1e0543f926c9da8099865a11
nrsanchez2450 <@TabulaRasa02>
2023-06-05 11:57:47 +0900

    switched to stack

--- client/src/SignIn.tsx
+++ client/src/SignIn.tsx
@@ -1,32 +1,20 @@
 import React from "react";
-import {Button, Grid, Stack, TextField, Typography} from '@mui/material';
+import {Button, Stack, TextField, Typography} from '@mui/material';
 
 
 export default function SignIn() {
   return (
-    <Grid container spacing={2.5} justifyContent={'center'} mt={'20%'} className="grid">
-
-      <Grid item xs={7}>
-        <Typography variant="h4">Sign in</Typography>
-      </Grid>
-
-      <Grid item xs={7}>
+    <Stack m={'auto'} mt={'12.5%'} width={'40%'} spacing={2.5}>
+      <Typography variant="h4">Sign in</Typography>
+      <div>
         <Typography variant="h6">Username</Typography>
         <TextField fullWidth required placeholder="Enter your username"></TextField>
-      </Grid>
-
-
-      <Grid item xs={7}>
-        <div><Typography variant="h6">Password</Typography>
-        <TextField fullWidth required placeholder="••••••••"></TextField></div>
-      </Grid>
-
-      <Grid item xs={7}><Button variant="contained" color="secondary" fullWidth>Sign In</Button></Grid>
-
-      <Grid item xs={7}>
-        <Typography>Don't have an account?<Button variant="text" color="secondary">Sign up</Button></Typography>
-      </Grid>
-
-    </Grid>
+      </div>
+      <div><Typography variant="h6">Password</Typography>
+      <TextField fullWidth required placeholder="••••••••"></TextField></div>
+      
+            <Button variant="contained" color="secondary" fullWidth>Sign In</Button>
+      <Typography>Don't have an account?<Button variant="text" color="secondary">Sign up</Button></Typography>
+    </Stack>
   );
 }
