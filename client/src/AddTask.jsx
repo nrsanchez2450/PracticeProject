1c0431eae706f43719ce327a9325e5a4c9ec4e12
jonahyad44 <103967416+jonahyad44@users.noreply.github.com>
2023-06-05 09:52:09 +0900

    Create AddTask.jsx

---
+++ client/src/AddTask.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddTodo() = () => {
+  return (
+    <form method= 'post'>
+      <input type= "text" name="text" placeholder= "Enter a task"/>
+      <button> Add </button>
+    </form>
+    }
+   }
+export fault  AddTodo
