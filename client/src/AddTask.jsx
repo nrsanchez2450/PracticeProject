a1714bad1044c96d879cf5651db5827349e11312
jonahyad44 <103967416+jonahyad44@users.noreply.github.com>
2023-06-05 10:27:36 +0900

    Update AddTask.jsx

--- client/src/AddTask.jsx
+++ client/src/AddTask.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { useState } from 'react'
 
-const AddTodo() = () => {
+const AddTodo() = (props) => {
+  const [item, setItem]  = useState("")
+  
+  const handleChange =  (e) =>{
+    setItem(e.target.value)
+  }
+  
+  const handleChange =  (e) => {
+    e.preventDefault()
+    props.item
+  }
+  
   return (
-    <form method= 'post'>
-      <input type= "text" name="text" placeholder= "Enter a task"/>
-      <button> Add </button>
+    <form method= 'post' onSubmit=(handleSubmit)>
+      <input type= "text" name="text" value = {item} onChange= {handleChange} placeholder= "Enter a task"/>
+      <button type = 'submit'> Add </button>
     </form>
-    }
-   }
+    )
+  }
 export fault  AddTodo
