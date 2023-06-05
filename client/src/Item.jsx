c87580423d862fe29d727f86f72655af5ad738a2
jonahyad44 <103967416+jonahyad44@users.noreply.github.com>
2023-06-05 10:38:50 +0900

    Update Item.jsx

--- client/src/Item.jsx
+++ client/src/Item.jsx
@@ -3,6 +3,6 @@ import React from 'react'
 const Item = () => {
   return (
     <div className= 'todo-item'>
-      <input type = "checkbox" />
+      <input type = "checkbox" onChange=() =>  props.completed()}/>
 
-export default Item
+export default Item;
