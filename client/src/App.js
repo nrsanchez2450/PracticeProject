5ccf2aec0418ede97c8d602a911deeb9e124c2ce
jonahyad44 <103967416+jonahyad44@users.noreply.github.com>
2023-06-06 12:02:09 +0900

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,9 @@
+import React, {useState} from 'react';
 import './App.css';
 import './ToDoActions.js';
 
 
-function App() {
-  
-    
-      
-    
-  }
-  
-  return (
-    <div className="To-Do App">
-    
-      <header className="App-header">
-    <h1> To-Do List </h1>
-    
-    
-      </header>
-    </div>
-  );
-}
 
+  
+  
 export default App;
