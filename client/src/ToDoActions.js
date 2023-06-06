6c22d768778b375edfb7e35d34864b58e5cdcfd9
jonahyad44 <103967416+jonahyad44@users.noreply.github.com>
2023-06-06 09:06:25 +0900

    Update ToDoActions.js

--- client/src/ToDoActions.js
+++ client/src/ToDoActions.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import from '/App.css'
 
 const Todo = () => {
+  
+  const [newItem, setNewItem] = useState('');
+  
   return (
     <div className = "addTasks">
-      <input type = "text" placeholder = "Enter task" />
+      <input type = "text" placeholder = "Enter task" value = {newItem}
+        onChange= {(e) => setNewItem(e.target.value)}
+    
+    </div>
+    
+    <div className = "clear all">
+    <button className = "clear-all-btn"> Clear All Tasks </button>
+    </div>
