6f9112df254ab109f2d1b54f26db41748c704a2f
jonahyad44 <103967416+jonahyad44@users.noreply.github.com>
2023-06-06 11:54:20 +0900

    Update ToDoActions.js

--- client/src/ToDoActions.js
+++ client/src/ToDoActions.js
@@ -17,12 +17,31 @@ const Todo = () => {
     }
   }
   
+  const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: newItem
+    };
+
+      setItems(oldList => [...oldList, item]);
+      setNewItem("");
+
+      console.log(items);
+  
   return (
     <div className = "addTasks">
       <input type = "checkbox" placeholder = "Enter task" value = {newItem}
         onChange= {(e) => setNewItem(e.target.value)}/>
       <button type = "submit" onClick= {() => addItem()}> Add </button>
+
+    <ul>
+          {items.map(item => {
+            return(
+              <li key={item.id}>{item.value}</li>
+            )
+          })}
+        </ul>
   
     </div>
-  )
+  );
+}
 export default Todo;
