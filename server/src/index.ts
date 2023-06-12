59d37f3383291c931dd4636e2c12d2a230169762
jonahyad44 <103967416+jonahyad44@users.noreply.github.com>
2023-06-12 09:36:06 +0900

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -96,15 +96,10 @@ app.post("/clearTasks", async (req, res) => {
 
 app.put("/updateTask", async (req, res) => {
   const id = req.params;
-  const body = req.body;
-  const arr = [];
-  arr.push(String);
-  arr.push(id);
   const updatedTask = await prisma.task.update({
     where: {id: Number(id)} ,
-    data: {body: String(body)},
+    data: {completed: true},
   });
-  console.log(arr);
   res.send(updatedTask);
 });
 
